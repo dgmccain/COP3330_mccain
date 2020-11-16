2686ffa86c08915ad3f74b37e517cbb17b5051b6
David McCain <david.g.mccain@gmail.com>
2020-11-16 03:25:38 -0500

    minor change - rearranged structure of test cases

--- mccain_p4/src/TaskItemTest.java
+++ mccain_p4/src/TaskItemTest.java
@@ -64,47 +64,4 @@ class TaskItemTest {
         assertEquals("2020-01-01", itemTest.dueDate);
     }
     */
-
-    //======================================================================================
-    //add tests for if the list itself fails (ex. trying to remove an index beyond bounds...
-    //also, add tests for functions contained within the TaskList class...
-    //======================================================================================
-
-    //===================================================================
-    //The following should be tests for functions within the App class...
-    //===================================================================
-
-    @Test
-    public void TestUnsuccessfulMainMenuChoice() {
-        //enter a char or out-of-bounds int...
-    }
-
-    @Test
-    public void TestMainMenuChoice1() {
-        //test for if int 1 is entered...
-    }
-
-    @Test
-    public void TestForIfFileDoesNotLoad() {
-        //enter the file name of a txt file that does not exist...
-        //expect print to console confirming the file does not exist...
-    }
-
-    @Test
-    public void TestForIfFileLoads() {
-        //enter the file name of a txt file that already exists...
-        //expect print to console confirming the file exists/loaded...
-    }
-
-    @Test
-    public void TestMainMenuChoice2() {
-        //test for if int 2 is entered...
-    }
-
-    @Test
-    public void TestMainMenuChoice3() {
-        //test for if int 3 is entered...
-        //program should close...
-    }
-
 }
---
+++ mccain_p4/src/TaskListTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Scanner;
+
+class TaskListTest {
+    //======================================================================================
+    //add tests for if the list itself fails (ex. trying to remove an index beyond bounds...
+    //also, add tests for functions contained within the TaskList class...
+    //======================================================================================
+}

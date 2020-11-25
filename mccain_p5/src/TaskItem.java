8dd7d179743e7d94a55736c43f7187907c7274e9
David McCain <david.g.mccain@gmail.com>
2020-11-24 20:10:43 -0500

    added getters, setters, and input validation - but still need to review them all

--- mccain_p5/src/TaskItem.java
+++ mccain_p5/src/TaskItem.java
@@ -4,18 +4,31 @@ public class TaskItem {
     private String dueDate;
     private boolean status; //status is complete/incomplete task status...
 
-    //default constructor...
+    //DEFAULT CONSTRUCTOR (for user input)...
     public TaskItem(String title, String description, String dueDate) {
-        this.title = title;
+        if (isTitleValid(title)) {
+            this.title = title;
+        }
+        else {
+            throw new IllegalArgumentException("INVALID - you must enter a title");
+        }
+
         this.description = description;
-        this.dueDate = dueDate;
+
+        if (isDueDateValid(dueDate)) {
+            this.dueDate = dueDate;
+        }
+        else {
+            throw new IllegalArgumentException("INVALID - you must enter a valid due date");
+        }
+
         this.status = false;
         //TaskApp should not retrieve status from user input when a new task
         //item is created (because new tasks should not be set as complete),
-        //so the default status should be set to false (incomplete).
+        //so the default status should be set to false (incomplete)...
     }
 
-    //constructor with status included as a parameter...
+    //CONSTRUCTOR (with status included as a parameter)...
     public TaskItem(String title, String description, String dueDate, boolean status) {
         this.title = title;
         this.description = description;
@@ -23,6 +36,74 @@ public class TaskItem {
         this.status = status;
     }
 
-    //add setters, create conditions for title and dueDate, and maybe
-    //getters - need to see if getters would be helpful/used...
+    //INPUT VALIDATION...
+    public boolean isTitleValid(String title) {
+        //check if title is empty...
+        return !title.matches("");
+    }
+    public boolean isDueDateValid(String dd) {
+        //check if due date match yyyy-MM-dd format...
+        //if statements ensure that months are 01-12 and days are 01-32...
+        if (dd.matches("[0-9]{4}[-][0-1][0-9][-][0-3][0-9]")) {
+            //if month == 0...
+            if (dd.matches("[0-9]{4}[-][0][0][-][0-3][0-9]")) {
+                return false;
+            }
+            //if month >= 13...
+            else if (dd.matches("[0-9]{4}[-][1][3-9][-][0-3][0-9]")) {
+                return false;
+            }
+            //if days == 0...
+            else if (dd.matches("[0-9]{4}[-][0-1][0-9][-][0][0]")) {
+                return false;
+            }
+            //if days >= 32...
+            else if (dd.matches("[0-9]{4}[-][0-1][0-9][-][3][2-9]")) {
+                return false;
+            }
+            //else should return all other valid dates (months 01-12 and days 01-31)...
+            else{
+                return true;
+            }
+        } else {
+            //input did not match yyyy-MM-dd format...
+            return false;
+        }
+    }
+
+    //SETTERS...
+    //functions go here...
+    public void setTitle(String title){
+        if(isTitleValid(title)) {
+            this.title = title;
+        }
+        else {
+            throw new IllegalArgumentException("You must enter a title...");
+        }
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setDueDate(String dueDate){
+        if(isDueDateValid(dueDate)) {
+            this.dueDate = dueDate;
+        }
+        else {
+            throw new IllegalArgumentException("You must enter a valid due date...");
+        }
+    }
+
+    //GETTERS - need to see if getters would be helpful/used...
+    public String getTitle() {
+        return this.title;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public String getDueDate() {
+        return this.dueDate;
+    }
+    public boolean getStatus() {
+        return this.status;
+    }
 }
